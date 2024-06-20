9ca142365f9bb8f6f31e8c7b3e123ea62c5d7dbb
Sanika Modak <sanika.211423.it@mhssce.ac.in>
2024-06-20 21:11:25 +0530

    Initial commit

---
+++ Desktop/Java_Assignment/src/com/Sanika_Modak/assignment/employees/Manager.java
@@ -0,0 +1,13 @@
+package com.Sanika_Modak.assignment.employees;
+
+/**
+ * Represents a manager with an additional department attribute.
+ */
+public class Manager extends Employee {
+    private String department;
+
+   
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
